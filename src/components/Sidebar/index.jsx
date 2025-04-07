fb71fa7c13dcc307adbd89699c2a968a53f68b4d
JonathanGRichards <110139851+JonathanGRichards@users.noreply.github.com>
2025-04-08 00:00:44 +0100

    add sidebar

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,12 +1,42 @@
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
-import LogoJR from '../../assets/images/JR_logo.png'
+import LogoJR from '../../assets/img/JR_logo.svg'
+import LogoSubJonat from '../../assets/img/Jonathan_sub.svg'
+import LogoSubRicha from '../../assets/img/Richards_sub.svg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
 
-const SidebarNav = () => {
-  ;<div className="side-navbar">
-    <Link className="" to "/">
+const Sidebar = () => (
+  <div className="nav-bar">
+    <Link className="logo" to="/">
       <img src={LogoJR} alt="logo" />
-      <img src={LogoJR} alt="logo" />
+      <div className="sub-logo">
+        <img className="sub-logo-jonathan" src={LogoSubJonat} alt="Jonathan" />
+        <img className="sub-logo-richards" src={LogoSubRicha} alt="Jonathan" />
+      </div>
     </Link>
+    <nav>
+      <NavLink exact="true" activeclassname="active" to="/">
+        <FontAwesomeIcon icon={faHome} />
+      </NavLink>
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className="about-link"
+        to="/about"
+      >
+        <FontAwesomeIcon icon={faUser} />
+      </NavLink>
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className="contact-link"
+        to="/contact"
+      >
+        <FontAwesomeIcon icon={faEnvelope} />
+      </NavLink>
+    </nav>
   </div>
-}
+)
+
+export default Sidebar
